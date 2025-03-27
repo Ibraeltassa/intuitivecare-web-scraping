444d85055343df3353b97bc8fc3f4ac03fc1cd87
Ibrahim <ibrahimeltassa@gmail.com>
2025-03-27 01:53:46 -0300

    feat: compacta os arquivos baixados em .Zip

--- src/main/java/com/intuitivecare/scraper/WebScraper.java
+++ src/main/java/com/intuitivecare/scraper/WebScraper.java
@@ -5,15 +5,18 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
 public class WebScraper {
     public static void main(String[] args) {
         System.out.println("Iniciando o Web Scraping");
 
         try {
+            // URL da página com os anexos PDF
             String url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos";
+
+            // Faz a requisição e obtém o conteúdo HTML da página
             Document doc = Jsoup.connect(url).get();
 
+            // Seleciona os links que terminam com ".pdf"
             Elements links = doc.select("a[href$=.pdf]");
 
             for (Element link : links) {
@@ -29,6 +32,13 @@ public class WebScraper {
                     FileDownloader.downloadFile(pdfUrl, "Anexo_II.pdf");
                 }
             }
+
+            // Após baixar os arquivos, compactar
+            String[] arquivos = {
+                    "downloads/Anexo_I.pdf",
+                    "downloads/Anexo_II.pdf"
+            };
+            ZipCompressor.zipFiles(arquivos, "downloads/Anexos_Completos.zip");
 
         } catch (Exception e) {
             System.out.println("Erro ao acessar a página: " + e.getMessage());
--- src/main/java/com/intuitivecare/scraper/ZipCompressor.java
+++ src/main/java/com/intuitivecare/scraper/ZipCompressor.java
@@ -1,4 +1,20 @@
 package com.intuitivecare.scraper;
 
+import net.lingala.zip4j.ZipFile;
+import java.io.File;
+
 public class ZipCompressor {
+    public static void zipFiles(String[] filesToZip, String outputZipPath) {
+        try {
+            ZipFile zipFile = new ZipFile(outputZipPath);
+
+            for (String filePath : filesToZip) {
+                zipFile.addFile(new File(filePath));
+            }
+
+            System.out.println("Arquivos compactados com sucesso em: " + outputZipPath);
+        } catch (Exception e) {
+            System.out.println("Erro ao compactar arquivos: " + e.getMessage());
+        }
+    }
 }
